d10e83d6dcd4f19231e8c43fd1d10e2d15dfef13
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-08-12 14:02:58 +0300

    Update studio.h

--- engine/studio.h
+++ engine/studio.h
@@ -369,10 +369,4 @@ typedef struct
 	short		s,t;		// s,t position on skin
 } mstudiotrivert_t;
 
-typedef struct
-{
-	vbomesh_t		*meshes;			// meshes per submodel
-	int		nummesh;			// mstudiomodel_t->nummesh
-} msubmodel_t;
-
 #endif//STUDIO_H
